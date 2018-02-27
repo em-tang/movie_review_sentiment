2edfae4117c72cfaae0dee94c091552549c4655f
em-tang <emt4@princeton.edu>
2018-02-27 12:41:23 -0500

    k-fold cross validation and more classifiers

--- classify.py
+++ classify.py
@@ -2,53 +2,55 @@ from sklearn import decomposition
 from sklearn import datasets
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_curve
+
 # import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# def naive_bayes:
+def multinomial_nb(x_train, y_train, x_test, y_test):
+  mnb = MultinomialNB()
+  mnb.fit(x_train, y_train)
+  return mnb.score(x_test, y_test)
 
+def gaussian_nb(x_train, y_train, x_test, y_test):
+  gnb = GaussianNB()
+  gnb.fit(x_train, y_train)
+  return gnb.score(x_test, y_test)
+
+def linear_svc(x,y):
+  lsvc = LinearSVC(random_state=0)
+  return cross_val_score(lsvc, x, y, cv = 10, scoring = 'accuracy')
 
 def k_nearest_neighbors(x_train, y_train, x_test, y_test):
-# knn = KNeighborsClassifier(n_neighbors=3)
-  return
+  knn = KNeighborsClassifier(n_neighbors=3)
+  knn.fit(x_train, y_train)
+  return knn.score(x_test, y_test)
 
 def logistic_regression(x_train, y_train, x_test, y_test):
   lr = LogisticRegression()
   lr.fit(x_train, y_train)
   return lr.score(x_test, y_test)
 
-def svm():
-  y = np.zeros(2400)
-  y[1201:] = 1
-  mean_vec = np.mean(x, axis=0)
-  cov_mat = (x - mean_vec).T.dot((x - mean_vec)) / (x.shape[0]-1)
-  print('Covariance matrix \n%s' %cov_mat)
-  # correlation = np.corrcoef(cov_mat.T)
-  evals, evecs = np.linalg.eig(cov_mat)
 
 
-  # Make a list of (eigenvalue, eigenvector) tuples
-  epairs = [(np.abs(evals[i]), evecs[:,i]) for i in range(len(evals))]
-
-  # Sort the (eigenvalue, eigenvector) tuples from high to low
-  epairs.sort()
-  epairs.reverse()
-
-  # Visually confirm that the list is correctly sorted by decreasing eigenvalues
-  print('Eigenvalues in descending order:')
-  for i in epairs:
-        print(i[0])
-
-# def 
 if __name__ == '__main__':
   x_train = pd.read_csv(filepath_or_buffer='train_bag_of_words_5.csv', header=None, sep=',').values
   y_train = pd.read_csv(filepath_or_buffer='train_classes_5.txt', header=None, sep=',').values
   x_test = pd.read_csv(filepath_or_buffer='test_bag_of_words_0.csv', header=None, sep=',').values
   y_test = pd.read_csv(filepath_or_buffer='test_classes_0.txt', header=None, sep=',').values
-  
-  print((y_train.shape))
-  print(y_test.shape)
-  
+
+
+  x = np.concatenate((x_train,x_test), axis=0)
+  y = np.concatenate((y_train,y_test), axis=0).ravel()
   print("read in data")
+
+  print(linear_svc(x,y))
+  # cross_validation(x_train, y_train, x_test, y_test)
   # print(logistic_regression(x_train,y_train,x_test,y_test))
+  # print(k_nearest_neighbors(x_train, y_train, x_test, y_test))
+  # print(gaussian_nb(x_train, y_train, x_test, y_test))
